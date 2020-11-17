2061ad54e24edfc5ddd956d2d9b2e2d5937ca7f0
Sebastián Díaz Obando <sebascout01182@gmail.com>
2020-11-17 17:02:21 -0600

    Cambiar Cache

--- Main.cpp
+++ Main.cpp
@@ -46,41 +46,43 @@ void Clientes(){
 		archivo.close();
 		archivo.open("Clientes.txt",ios::in);
 		int lineas[largo];
+		string lineasNom[largo];
 		int z = 0;
     	while(!archivo.eof()){
     		getline(archivo,texto);
 			int posPC = texto.find(";");
 	        int cedula = atoi(texto.substr(0, posPC).c_str()); string nombre = texto.substr(posPC + 1, texto.length());
-    		lineas[z] = cedula;
-    		z++;
+			lineas[z] = cedula;
+			lineasNom[z] = nombre;
+			z++;
     	}
     	archivo.close();
-    	/*
     	int i,j,k;
-    	int tam = largo;
+    	int tam = largo-1;
     	for(i = 0; i < tam; i++){
 			for(j = i+1; j < tam; j++){
 				if(lineas[i] == lineas[j]){
 					k = j;
 					while(k < tam){
 						lineas[k] = lineas[k+1];
+						lineasNom[k]= lineasNom[k+1];
 						++k;
 					}
 					--tam;
 					--j;
 				}
 			}
+		//cout<<"Cedula: "<<lineas[i]<<" Nombre: "<<lineasNom[i]<<endl;
 		}
-		*/
 		int indice = 0;
 		for(int x = 0;x<tam;x++){
 			Indice(lineas[x],indice);
 		}
-		remove("Clientes.txt");
  		ofstream archivo;
-    	archivo.open("Clientes.txt", ios::app);
-    	for(x=0;x<tam;x++){
-    		archivo<<lineas[x]<<endl;
+    	archivo.open("ClientesRN.txt", ios::app);
+    	for(int x = 0;x<tam;x++){
+    		archivo<<lineas[x]<<";"<<lineasNom[x]<<endl;
+    		
 		}
 	}
 }
@@ -112,7 +114,7 @@ pNodoBinario CrearArbol(){
 void CrearCache(int cacheInt[],string cacheStr[]){
 	ifstream archivo;
 	string texto;
-	archivo.open("Clientes.txt",ios::in);
+	archivo.open("ClientesRN.txt",ios::in);
 	if (archivo.fail()){
 	    cout<<"No se pudo abrir el archivo";
 	    exit(1);
@@ -143,42 +145,86 @@ void MostrarCache(int cacheInt[],string cacheStr[]){
 	}
 }
 
+
+bool ExisteCliente(pNodoBinario &raiz, int cedula, int &indice){
+	if(raiz==NULL){
+        return false;
+    }
+	else if (raiz->valor== cedula) {
+		indice= raiz->indice;
+		return true; 
+    }
+	else if(cedula<raiz->valor){
+    	ExisteCliente(raiz->Hizq,cedula,indice);
+	}
+	else{
+		ExisteCliente(raiz->Hder,cedula,indice);
+	}
+}
+
+
 void EliminarCliente(){
 	int cedula; cout<<"Ingrese la cedula del usuario a elminar: "; cin>>cedula; cout<<endl;
+}
+
+bool BuscarEnCache(int cedula, int cacheInt[], string cacheStr[], string &nombre){
+	for(int x=0; x<20; x++){
+		if(cacheInt[x] == cedula){
+			nombre = cacheStr[x];
+			return true;
+		}
+	}
+	return false;
+}
+void CambiarCache(int indice, string &nombre, int cacheInt[], string cacheStr[]){
+	
+}
+
+void BuscarCliente(pNodoBinario &raiz , int cacheInt[], string cacheStr[]){
+	int cedula; cout<<"Ingrese la cedula del usuario a buscar: "; cin>>cedula; cout<<endl;
+	int indice; string nombre;
+	if(ExisteCliente(raiz,cedula,indice)){
+		if(BuscarEnCache(cedula,cacheInt,cacheStr,nombre)){
+			cout<<"El cliente solicitado es "<< nombre<< " de cedula "<<cedula<<endl;	
+		}else{
+			//Renovar Cache
+		}
+	}else{
+		cout<<"El usuario ingresado no existe"<<endl;
+	}
 }
 
 int main(){
 	int opcion;
 	int cacheInt[20];string cacheStr[20];
-	CrearCache(cacheInt,cacheStr);
 	pNodoBinario raiz;
+	Clientes(); 
+	raiz = CrearArbol();
+	CrearCache(cacheInt,cacheStr);
 	do { 
 		system("cls");      // Para limpiar la pantalla 
 		cout << "Tarea Corta #3" << endl; 
 		cout << "\nMenu" << endl<<endl; 
-		cout << "1. Cargar los archivos" << endl;
-		cout << "2. Memoria cache" << endl; 
-		cout << "3. Busqueda" << endl; 
-		cout << "4. Insertar Cliente" << endl; 
-		cout << "5. Eliminar " << endl; 
-		cout << "6. Purgar " << endl; 
-		cout << "7. Reindexar " << endl; 
-		cout << "8. Salir" << endl; 
+		cout << "1. Memoria cache" << endl; 
+		cout << "2. Busqueda" << endl; 
+		cout << "3. Insertar Cliente" << endl; 
+		cout << "4. Eliminar " << endl; 
+		cout << "5. Purgar " << endl; 
+		cout << "6. Reindexar " << endl; 
+		cout << "7. Salir" << endl; 
 		cout << "\nIngrese una opcion: "; 
 		cin >> opcion; 
 		cout<<endl; 
 		switch (opcion) {  
 		    case 1:
-		    	Clientes(); 
-				raiz = CrearArbol();
-				cout<<"Los archivos se han cargado exitosamente"<<endl;
 				system("pause>nul"); 
-				break;  
+				break;
 		    case 2:
 		    	MostrarCache(cacheInt,cacheStr);
 		    	system("pause>nul"); 
 		        break;    
 			case 3:
+				BuscarCliente(raiz , cacheInt, cacheStr);
 		        system("pause>nul"); 
 		        break;                 
 		 	case 4:
@@ -190,13 +236,10 @@ int main(){
 		        break;  
 			case 6:
 		        system("pause>nul"); 
-		        break; 
-			case 7:
-		        system("pause>nul"); 
-		        break;        
+		        break;       
 		}	
 	}
-	while (opcion!=8);
+	while (opcion!=7);
 		cout<<"Gracias por utilizar el programa"<<endl;			
    return 0;
 }
